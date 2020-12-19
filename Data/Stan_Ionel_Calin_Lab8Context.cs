351066f3dfffc88561e9f6f93decb4badbad7b27
ionel.calin.stan@gmail.com <ionel.calin.stan@gmail.com>
2020-12-19 17:19:13 +0200

    Add project files.

---
+++ Data/Stan_Ionel_Calin_Lab8Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Stan_Ionel_Calin_Lab8.Models;
+
+namespace Stan_Ionel_Calin_Lab8.Data
+{
+    public class Stan_Ionel_Calin_Lab8Context : DbContext
+    {
+        public Stan_Ionel_Calin_Lab8Context (DbContextOptions<Stan_Ionel_Calin_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Stan_Ionel_Calin_Lab8.Models.Book> Book { get; set; }
+    }
+}
---
+++ Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Stan_Ionel_Calin_Lab8.Data;
+using Stan_Ionel_Calin_Lab8.Models;
+
+namespace Stan_Ionel_Calin_Lab8.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Stan_Ionel_Calin_Lab8.Data.Stan_Ionel_Calin_Lab8Context _context;
+
+        public IndexModel(Stan_Ionel_Calin_Lab8.Data.Stan_Ionel_Calin_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
